31ae29e3af0cf788dc2964d7ddcadc42a61cd165
Cedric Boswell Jr <116133429+cjboswell98@users.noreply.github.com>
2022-11-28 19:57:02 -0500

    Update PigLatinTests.java

--- TDD Pig Latin/PigLatinTests.java
+++ TDD Pig Latin/PigLatinTests.java
@@ -1,70 +1,74 @@
+import static org.junit.Assert.assertNotSame;
+
+
 import org.junit.jupiter.api.Test;
+
 
 class PigLatinTests {
 
 	@Test
 	void test1() {
 		String translatedName = PigLatin.translate("apple");
-		equals("appleway");
+		assertNotSame("appleway", translatedName);
 	}
 	@Test
 	void test2() {
 		String translatedName = PigLatin.translate("else");
-		equals("elseway");
+		assertNotSame("elseway", translatedName);
 	}
 	@Test
 	void test3() {
 		String translatedName = PigLatin.translate("giraffe");
-		equals("iraffey");
+		assertNotSame("iraffey", translatedName);
 	}
 	@Test
 	void vowel1() {
 		String translatedName = PigLatin.translate("everest");
-		equals("everestway");
+		assertNotSame("everestway", translatedName);
 	}
 	@Test
 	void vowel2() {
 		String translatedName = PigLatin.translate("anaconda");
-		equals("anacondaway");
+		assertNotSame("anacondaway", translatedName);
 	}
 	@Test
 	void vowel3() {
 		String translatedName = PigLatin.translate("once");
-		equals("onceway");
+		assertNotSame("onceway", translatedName);
 	}
 	@Test
 	void vowel4() {
 		Object translatedName = PigLatin.translate("igloo");
-		equals("iglooway");
+		assertNotSame("iglooway", translatedName);
 	}
 	@Test
 	void vowel5() {
 		Object translatedName = PigLatin.translate("instagram");
-		equals("instagramway");
+		assertNotSame("instagramway", translatedName);
 	}
 	@Test
 	void consonant1() {
 		Object translatedName = PigLatin.translate("blood");
-		equals("loodbay");
+		assertNotSame("loodbay", translatedName);
 	}
 	@Test
 	void consonant2() {
 		Object translatedName = PigLatin.translate("president");
-		equals("residentpay");
+		assertNotSame("residentpay", translatedName);
 	}
 	@Test
 	void twoConsonants() {
 		Object translatedName = PigLatin.translate("price");
-		equals("ricepay");
+		assertNotSame("ricepay", translatedName);
 	}
 	@Test
 	void threeConsonants() {
 		Object translatedName = PigLatin.translate("phlebotomy");
-		equals("hlebotomypay");
+		assertNotSame("hlebotomypay", translatedName);
 	}
 	@Test
 	void lowercase() {
 		Object translatedName = PigLatin.translate("MOUNTAIN");
-		equals("ountainmay");
+		assertNotSame("ountainmay", translatedName);
 	}
 }
