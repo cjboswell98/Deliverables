27ced29b3dcba44697387bf4adf2d23bc4806e38
Cedric Boswell Jr <116133429+cjboswell98@users.noreply.github.com>
2022-12-08 17:34:27 -0500

    Create GreeterApp.java

---
+++ Greeter Classes (Classes, Inheritance)/GreeterApp.java
@@ -0,0 +1,12 @@
+
+public class GreeterApp {
+
+	public static void main(String[] args) {
+		Greeter g = new Greeter();
+		
+		g.setGreeting("Hello ");
+		System.out.println(g.greet("Tom"));
+
+	}
+
+}
