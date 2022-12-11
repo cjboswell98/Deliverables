38c4c817712e5a2c87299b4f1b31c0047ef9c0f2
Cedric Boswell Jr <116133429+cjboswell98@users.noreply.github.com>
2022-12-11 14:24:38 -0500

    Update Greeter.java

--- Greeter Classes (Classes, Inheritance)/Greeter.java
+++ Greeter Classes (Classes, Inheritance)/Greeter.java
@@ -1,19 +1,23 @@
 
 public class Greeter {
+	//field
 	String greeting;
 	
 	public void setGreeting(String greeting) {
+		//setter
 		this.greeting = greeting;
 	}
 	
 	public String getGreeting() {
+		//getter
 		return greeting;
 	}
 
 	public String greet(String name) {
-		return greeting + name + "!";
+		return greeting + " " + name + "!";
 	}
-	public Greeter() {
-		
+	//constructor
+	public Greeter(String greeting) {
+		this.greeting = greeting;
 	}
 }
