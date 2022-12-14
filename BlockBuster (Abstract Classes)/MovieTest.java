c9815ca125c605b3bf6c2293c5587a4a469cbf5b
Cedric Boswell Jr <116133429+cjboswell98@users.noreply.github.com>
2022-12-14 18:31:00 -0500

    Create MovieTest.java

---
+++ BlockBuster (Abstract Classes)/MovieTest.java
@@ -0,0 +1,50 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MovieAppTest {
+
+	@Test
+	void testDVDtitle() {
+		DVD title = new DVD("Star Wars",136 );
+		String actual = title.getTitle();
+		String expected = "Star Wars";
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testDVDrunTime() {
+		DVD runTime = new DVD("Star Wars",136 );
+		int actual = runTime.getRunTime();
+		int expected = 136;
+		assertEquals(expected, actual);
+	}
+	@Test
+	void testVHStitle() {
+		VHS title = new VHS("Private Ryan", 115);
+		String actual = title.getTitle();
+		String expected = ("Private Ryan");
+		assertEquals(expected, actual);
+	}
+	@Test
+	void testVHSrunTime() {
+		VHS runTime = new VHS("Private Ryan", 115);
+		int actual = runTime.getRunTime();
+		int expected = 115;
+		assertEquals(expected, actual);
+	}
+	@Test
+	void testVHScurrentTime() {
+		VHS currentTime = new VHS("Private Ryan", 115);
+		double actual = currentTime.getCurrentTime();
+		double expected = 0.0;
+		assertEquals(expected, actual);
+	}
+	@Test
+	void testVHSrewind() {
+		VHS currentTime = new VHS("Private Ryan", 115);
+		double actual = currentTime.
+		double expected = 0.0;
+		assertEquals(expected, actual);
+	}
+}
